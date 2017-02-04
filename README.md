a1c9a05e02635e44b31dadc74b728500c8232b99
Deepika Koduri <sumanthratakundla@Sumanths-MacBook-Air.local>
2017-02-04 13:21:23 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A boilerplate to start building your React apps with Bootstrap and Webpack.
---
+++ app/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from 'react-bootstrap';
+const buttonsInstance = (
+  <Button>Hello World</Button>
+);
+
+ReactDOM.render(buttonsInstance, document.getElementById('app'));
