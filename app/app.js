02d7bbc213f3f9659ea103e8208461c5f5fb7c4f
Deepika Koduri <sumanthratakundla@Sumanths-MacBook-Air.local>
2017-02-07 22:51:52 -0600

    added bootstrap css

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
 const buttonsInstance = (
   <Button>Hello World</Button>
 );
